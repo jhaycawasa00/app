c3d079f403c44b963d9058dc5a29a8d14aad14c1
jhay <pipzhermogenes@gmail.com>
2019-12-31 19:36:31 +0800

    Updates

---
+++ precache-manifest.fbf88d28122492fa5af6aebc7df6901c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab163e7a7cd00e60c1ab25a6942a666",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d6b4608e78e76f1a4bc8",
+    "url": "/app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3d1cfd0833e8a52b8f92",
+    "url": "/app/static/js/2.3eed503d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app/static/js/2.3eed503d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6b4608e78e76f1a4bc8",
+    "url": "/app/static/js/main.75656d7c.chunk.js"
+  },
+  {
+    "revision": "054e601e0edc84b152b2",
+    "url": "/app/static/js/runtime-main.3c55a1bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
